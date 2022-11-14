1ab6d622d05b56cf5fa514a811cf0c48745eba93
gavinlampton <60271526+gavinlampton@users.noreply.github.com>
2022-11-13 19:16:42 -0800

    Close to getting a basic version of client/server udp to work. Just need to debug.

--- clientTest.py
+++ clientTest.py
@@ -1,2 +1,17 @@
 import socket
 
+defaultDestination = "127.0.0.1"
+defaultDestinationPort = 4444
+defaultPort = 42069
+
+s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+try:
+    s.bind( ("", defaultPort) )
+    buffer = b"test"
+
+    print("sending data")
+    s.sendto(buffer, (defaultDestination,defaultDestinationPort))
+    data = s.recv(1000)
+    print(str(data))
+finally:
+    s.close()
--- serverTest.py
+++ serverTest.py
@@ -1,12 +1,17 @@
 import socket
+
+
 port = 4444
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", port))
 print("waiting on port:", port)
 
+try:
+    data, addr = s.recv(1000)
+    print("Received {0}" % data)
+    data = str.capitalize(str(data))
+    s.sendto(bytes(data), addr)
 
-data, addr = s.recvfrom(1024)
-print(data)
-
-s.close()
+finally:
+    s.close()
