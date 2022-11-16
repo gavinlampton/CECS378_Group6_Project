8cae0e500b769ec609ea5a6bf84bee6a5bcd313e
gavinlampton <60271526+gavinlampton@users.noreply.github.com>
2022-11-16 01:39:33 -0800

    Tried to make the test.py compile into a smaller file but it didn't work. added the new test folder to gitignore.

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 #!/bin/python3
-import os
-import sys
-import random
+from os import execv
+from sys import argv
+from random import randint
 print("Have a random number: ")
-print(random.randint(0, 10000))
-os.execv(sys.argv[0], sys.argv)
+print(randint(0, 10000))
+execv(argv[0], argv)
 
